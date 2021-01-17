be073b811b99ae2715d4f1879fe72f75fb39f5bc
Molka <molkabenameur6@gmail.com>
2021-01-17 20:09:14 +0100

    third

--- app0/urls.py
+++ app0/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
 
      path(r'^search/$', views.search, name='search'),
      url(r'^(?P<user_id>[0-9]+)/$', views.detail_element, name='detail_name'),
-     # path('<int:user_id>/', views.detail_element, name='detail_name')
      path(r'^ajouter/$', views.ajouter, name='ajouter'),
+       # path('<int:user_id>/', views.detail_element, name='detail_name')
 
 
 
